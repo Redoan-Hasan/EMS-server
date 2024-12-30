4ce1338f5f4bee5c1510f96a9eca4a5989631043
Redoan Hasan <redoanhasan57@gmail.com>
2024-12-30 21:07:14 +0600

    got some issue in the frontend after deploying , gonna fix those

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors({ origin: ["http://localhost:5000","http://localhost:5173"] }));
+app.use(
+  cors({
+    origin: [
+      "https://ems-server-eight.vercel.app",
+      "http://localhost:5173",
+      "https://https://ems-server-eight.vercel.app/",
+    ],
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhz4s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,25 +31,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const taskCollection = client.db("EMS").collection("EMS-tasks");
     const employeeCollection = client.db("EMS").collection("EMS-employees");
-    const completedTaskCollection = client.db("EMS").collection("EMS-completedTasks");
-    
-    // getting all the pending tasks 
+    const completedTaskCollection = client
+      .db("EMS")
+      .collection("EMS-completedTasks");
+
+    // getting all the pending tasks
     app.get("/allPendingTasks", async (req, res) => {
       const result = await taskCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    // getting all the completed tasks 
+    // getting all the completed tasks
     app.get("/allCompletedTask", async (req, res) => {
-      const result = await completedTaskCollection.find().toArray();      
+      const result = await completedTaskCollection.find().toArray();
       res.send(result);
-    })
-
-
+    });
 
     app.post("/completedTask", async (req, res) => {
       const completedTask = req.body;
@@ -52,10 +60,10 @@ async function run() {
 
     app.get("/completedTask", async (req, res) => {
       let query = {};
-      if(req.query?.email){
-        query = {email:req.query.email};
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
-      const cursor = completedTaskCollection.find(query).sort({priority:1});
+      const cursor = completedTaskCollection.find(query).sort({ priority: 1 });
       const tasks = await cursor.toArray();
       res.send(tasks);
     });
@@ -68,7 +76,7 @@ async function run() {
     });
 
     app.get("/addEmployee", async (req, res) => {
-      const cursor =await employeeCollection.find().toArray();
+      const cursor = await employeeCollection.find().toArray();
       res.send(cursor);
     });
 
@@ -81,10 +89,10 @@ async function run() {
 
     app.get("/addTask", async (req, res) => {
       let query = {};
-      if(req.query?.email){
-        query = {email:req.query.email};
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
-      const cursor = taskCollection.find(query).sort({priority:1});
+      const cursor = taskCollection.find(query).sort({ priority: 1 });
       const tasks = await cursor.toArray();
       res.send(tasks);
     });
@@ -92,20 +100,20 @@ async function run() {
     app.get("/addTask/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = { _id :new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const task = await taskCollection.findOne(query);
       res.send(task);
     });
 
     app.delete("/addTask/:id", async (req, res) => {
       const id = req.params.id;
-        const query = { _id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
